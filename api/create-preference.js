158b40e39bf2650949b8f5f585e7530d30bcfdf9
Fran Teijeiro <franciscoteijeiro@gmail.com>
2026-02-08 16:45:44 -0300

    Update create-preference.js

--- api/create-preference.js
+++ api/create-preference.js
@@ -1,11 +1,3 @@
-module.exports = function handler(req, res) {
-  res.status(200).json({ ok: true });
-};
-    res.status(200).json({
-      init_point: response.body.init_point,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "MercadoPago error" });
-  }
+export default function handler(req, res) {
+  res.status(200).send("OK");
 }
